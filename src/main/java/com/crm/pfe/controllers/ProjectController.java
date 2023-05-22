414712afce611fc8b6e7dd7c64cf03086ec494c9
MohamedDhiaZoghlami <mohameddhia.zoghlami@esprit.tn>
2023-05-22 12:00:06 +0100

    project updated

--- src/main/java/com/crm/pfe/controllers/ProjectController.java
+++ src/main/java/com/crm/pfe/controllers/ProjectController.java
@@ -1,4 +1,40 @@
 package com.crm.pfe.controllers;
 
+import com.crm.pfe.entities.Project;
+import com.crm.pfe.services.ProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/project")
+@AllArgsConstructor
 public class ProjectController {
+    private final ProjectService projectService;
+
+    @PostMapping("/create/{idCustomer}")
+    public Project createProject(@PathVariable Long idCustomer, @RequestBody Project project) {
+        return projectService.createProject(idCustomer,project);
+    }
+
+    @GetMapping("/all")
+    public List<Project> getAllProjects() {
+        return projectService.getAllProjects();
+    }
+
+    @GetMapping("/{id}")
+    public Project getProjectById(@PathVariable Long id) {
+        return projectService.getProjectById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public Project updateProject(@PathVariable Long id, Project project) {
+        return projectService.updateProject(id,project);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProject(@PathVariable Long id) {
+        return projectService.deleteProject(id);
+    }
 }
--- src/main/java/com/crm/pfe/entities/Project.java
+++ src/main/java/com/crm/pfe/entities/Project.java
@@ -1,11 +1,14 @@
 package com.crm.pfe.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name="project")
@@ -20,4 +23,11 @@ public class Project {
     private String description;
     private Date start_date;
     private Date end_date;
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="customer_id")
+    @JsonIgnore
+    private Customer customer;
+
 }
