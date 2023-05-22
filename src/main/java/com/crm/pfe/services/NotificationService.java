2cdd9e93ef7dba70bf702b617ebdcc622a16e9fa
MohamedDhiaZoghlami <mohameddhia.zoghlami@esprit.tn>
2023-05-22 08:42:18 +0100

    some basic notification funcs

--- src/main/java/com/crm/pfe/services/NotificationService.java
+++ src/main/java/com/crm/pfe/services/NotificationService.java
@@ -1,4 +1,13 @@
 package com.crm.pfe.services;
 
+import com.crm.pfe.entities.Notification;
+
+import java.util.List;
+
 public interface NotificationService {
+    Notification createNotification(Long id,Notification notification);
+
+    Notification getNotificationById(Long id);
+
+    Notification updateNotification(Long id, Notification notification);
 }
