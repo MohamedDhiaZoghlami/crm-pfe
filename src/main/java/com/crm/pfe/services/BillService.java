ef457a55f40a885bf64a635ef4b6649fbf7d210f
MohamedDhiaZoghlami <mohameddhia.zoghlami@esprit.tn>
2023-05-22 12:21:38 +0100

    bills added

--- src/main/java/com/crm/pfe/services/BillService.java
+++ src/main/java/com/crm/pfe/services/BillService.java
@@ -1,4 +1,17 @@
 package com.crm.pfe.services;
 
+import com.crm.pfe.entities.Bill;
+
+import java.util.List;
+
 public interface BillService {
+    Bill createBill(Long id, Bill bill);
+
+    List<Bill> getAllBills();
+
+    Bill getBillById(Long id);
+
+    Bill updateBill(Long id, Bill bill);
+
+    String deleteBill(Long id);
 }
